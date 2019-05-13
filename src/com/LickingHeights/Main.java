63e55fc0e312ea5a072476a2c0d154294471eefe
Chris <christopher.emrick.2022@student.lhschools.org>
2019-05-13 13:42:44 -0400

    Added note

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -2,7 +2,9 @@ package com.LickingHeights;
 
 public class Main {
 
+    // Originally made this in another file
+    // That's why the time stamps are so close together
+
     public static void main(String[] args) {
-	// write your code here
     }
 }
