0e086b732a648fd99880579f1d6329b88fc51041
Chris <christopher.emrick.2022@student.lhschools.org>
2019-05-13 13:43:03 -0400

    Added getRandomChoice method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,4 +7,25 @@ public class Main {
 
     public static void main(String[] args) {
     }
+    private static int getRandomChoice() {
+        int [] choices = new int[4];
+        choices[1] = 1;
+        choices[2] = 2;
+        choices[3] = 3;
+        // Array looks like this:
+        // _______
+        // |1 2 3|
+        // |-----|
+        // |1 2 3|
+        int random = (int) (Math.random()*100);
+        if(random < 33) {
+            return choices[1]; // rock
+        }
+        else if(random >= 33 && random <= 66) {
+            return choices[2]; // paper
+        }
+        else {
+            return choices[3]; // scissors
+        }
+    }
 }
